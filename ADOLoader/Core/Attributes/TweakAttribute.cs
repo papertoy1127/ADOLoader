a0ff0dd570ea2604ef3a2e47f9c60520a6e47f13
papertoy1127 <richard1909@naver.com>
2021-08-02 18:07:47 +0900

    Tweak을 애트리뷰트로 바꿈

---
+++ ADOLoader/Core/Attributes/TweakAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ADOLoader.Core.Attributes {
+    public class TweakAttribute : Attribute {
+        // 트윅을 묶어서 정리할 수 있는 기능
+        
+    }
+}
--- ADOLoader/Core/Tweaks/Tweak.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ADOLoader.Core.Tweaks {
-    public abstract class Tweak {
-        internal static Dictionary<Type, Tweak> _tweakInstances = new();
-        
-    }
-}
